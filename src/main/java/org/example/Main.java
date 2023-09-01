674ce1aa34819654f3c61c9f3556c5a8b7810c53
hyungjin0105 <hyungjin015@handong.ac.kr>
2023-09-01 16:51:14 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("21800204", "김형진");
+
+        System.out.print(s1.toString());
+
+    }
+}
